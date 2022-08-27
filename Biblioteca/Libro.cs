06584aad3fc590aae956106b650485d0bafb971f
frankramirezp <112280552+frankramirezp@users.noreply.github.com>
2022-08-27 18:30:42 -0400

    Agregar archivos de proyecto.

---
+++ Biblioteca/Libro.cs
@@ -0,0 +1,44 @@
+﻿namespace Biblioteca
+{
+    public class Libro
+    {
+        public string? Codigo { get; set; }
+        public string? Nombre { get; set; }
+        public string? Autor { get; set; }
+        public string? Tipo { get; set; }
+        public int Cantidad { get; set; }
+        public int Precio { get; set; }
+        public string? Ubicacion { get; set; }
+        public double Cambio { get; set; }
+
+        public void sumarCantidad(int cantidad)
+        {
+            this.Cantidad += cantidad;
+        }
+
+        public bool validarCantidad()
+        {
+            if(this.Cantidad > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool validarPrecio(double precio)
+        {
+            if(this.Precio <= precio)
+            {
+                this.Cambio = precio - this.Precio;
+                return true;
+            }
+            return false;
+        }
+
+        public void restarLibro()
+        {
+            this.Cantidad--;
+        }
+
+    }
+}
---
+++ Biblioteca/Registradora.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Biblioteca
+{
+
+    public class Registradora
+    {
+        string ruta = @"C:\Users\f_ram\Documents\Proyecto Final\Sistema de control y venta de libros\Controllibros\Listado de libros.txt";
+        StreamWriter escribir;
+        
+        
+        public List<Libro> Libros { get; set; }
+        public double Pago { get; set; }
+
+        public Registradora()
+        {
+            this.Libros = new List<Libro>();
+
+            Libro maria = new Libro();
+            maria.Codigo = "01";
+            maria.Nombre = "Maria";
+            maria.Autor = "Jorge Isaac";
+            maria.Tipo = "Novela";
+            maria.Cantidad = 5;
+            maria.Precio = 500;
+            maria.Ubicacion = "A1";
+
+            Libro elalquimista = new Libro();
+            elalquimista.Codigo = "02";
+            elalquimista.Nombre = "El Alquimista";
+            elalquimista.Autor = "Paulo Coelho";
+            elalquimista.Tipo = "Novela";
+            maria.Cantidad = 2;
+            elalquimista.Precio = 1500;
+            elalquimista.Ubicacion = "A2";
+
+            this.Libros.Add(maria);
+            this.Libros.Add(elalquimista);
+        }
+
+        public int validaLibro(string codigo)
+        {
+            int encontro = -1;
+
+            for (int i = 0; i < this.Libros.Count; i++)
+            {
+                if (this.Libros[i].Codigo == codigo)
+                {
+                    encontro = i;
+                }
+            }
+            return encontro;
+        }
+
+        public bool agregarLibro(Libro libro)
+        {
+            int enc = this.validaLibro(libro.Codigo);
+            if (enc >= 0)
+            {
+                this.Libros[enc].sumarCantidad(libro.Cantidad);
+            }
+            else
+            {
+                this.Libros.Add(libro);
+            }
+
+            return true;
+        }
+
+        
+
+        public Libro vender(string codigo)
+        {
+            int enc = this.validaLibro(codigo);
+
+            if (enc >= 0)
+            {
+                if (this.Libros[enc].validarCantidad())
+                {
+                    if (this.Libros[enc].validarPrecio(Pago))
+                    {
+                        this.Libros[enc].restarLibro();
+                        return this.Libros[enc];
+                    }
+                    
+                }
+            }
+
+            return null;
+        }
+
+        public string listarLibro()
+        {
+            escribir = File.AppendText(ruta);
+            string lista = "";
+            foreach (Libro item in this.Libros)
+            {
+                lista += " Código: " + item.Codigo + " Nombre: " + item.Nombre + " Tipo: " + item.Tipo + " Autor: " + item.Autor + " Cantidad: " + item.Cantidad + " Precio: " + item.Precio + " Ubicación: " + item.Ubicacion + "\n";
+                escribir.WriteLine(lista);
+                
+            }
+            escribir.Flush();
+            escribir.Close();
+            return lista;
+        }
+
+        public string listarAutores()
+        {
+            string lista = "";
+            foreach (Libro item in this.Libros)
+            {
+                lista += " Autor: " + item.Autor + "\n";
+
+            }
+            return lista;
+        }
+    }
+}
